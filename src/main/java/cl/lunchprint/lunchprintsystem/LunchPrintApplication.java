c8c438ae276c770246591fb6f9ad5b63ed4dc790
Mauricio Gutierrez <mauricio.mgu@gmail.com>
2024-10-11 16:13:39 -0300

    Cargando proyecto springboot inicial

---
+++ src/main/java/cl/lunchprint/lunchprintsystem/LunchPrintApplication.java
@@ -0,0 +1,13 @@
+package cl.lunchprint.lunchprintsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchPrintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchPrintApplication.class, args);
+	}
+
+}
